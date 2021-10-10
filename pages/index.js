6e6f5bf16b73aef4148162f2e288e7976c29d720
Naveen Thurimerla <nawinto99@gmail.com>
2021-10-10 11:24:09 +0530

    :sparkles: change the site bg

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import Head from "next/head";
 import Header from "@components/Header";
-import Footer from "@components/Footer";
+//import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Data Dlog</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to Data Dlog" />
+        <img src="/datadlog.gif" alt="datadlog"/>
       </main>
-
-      <Footer />
     </div>
   );
 }
